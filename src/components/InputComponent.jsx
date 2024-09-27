1c45dcd3ff397c526998d56a8de5a630da21d677
mubbe127 <mubarek.tajudin@gmail.com>
2024-09-27 15:08:01 +0200

    testing deployment

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -9,7 +9,7 @@ function InputComponent({
   type = "input",
   placeholder,
   submit,
-  setSubmit
+  setSubmit,
 }) {
   const [value, setValue] = useState("");
   const [editMode, setEditMode] = useState(true);
@@ -19,27 +19,33 @@ function InputComponent({
   function handleChange(e) {
     setValue(e.target.value);
 
+    if (type === "email") {
+      console.log("hej")
+      if (e.target.validity.typeMismatch) {
+        e.target.setCustomValidity("I am expecting an email address!");
+      } else {
+        e.target.setCustomValidity("");
+      }
+    }
+
   }
   function handleFocus() {
     setEditMode(true);
   }
 
   function handleBlur(e) {
-   if(e.target.checkValidity()){
-    setEditMode(false);
-    return
-   }
+    if (e.target.checkValidity()) {
+      setEditMode(false);
+      return;
+    }
 
-   console.log("invalid")
-    
+    console.log("invalid");
   }
 
   function handleClick() {
-   
     setEditMode(true);
-    setSubmit(false)
+    setSubmit(false);
   }
-  
 
   useEffect(() => {
     if (isFirstRender.current) {
@@ -48,20 +54,18 @@ function InputComponent({
     }
     if (editMode && inputRef.current) {
       inputRef.current.focus();
-    
     }
   }, [editMode]);
 
-
-
   return (
     <div className={classNameContainer}>
+      <div><h1>JALLA MANNEN</h1></div>
       {value !== "" && (!editMode || submit) ? (
         <OutputElement className={classNameOutput} onClick={handleClick}>
           {value}
         </OutputElement>
       ) : (
-        <InputElement 
+        <InputElement
           ref={inputRef}
           className={classNameInput}
           type={type}
@@ -71,7 +75,6 @@ function InputComponent({
           onFocus={handleFocus}
           onBlur={handleBlur}
           required
-          
         />
       )}
     </div>
